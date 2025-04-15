a6f7d6b9a9d6918669a3bfa73999698c935e9c24
criis436 <cristian.zavala2006@gmail.com>
2025-04-15 15:38:20 -0400

    primer comit

---
+++ hola.py
@@ -0,0 +1,24 @@
+# # #declaracion de variables
+# # nombre="link"
+# # edad=64
+# # #ejemplo de concatenacion
+# # print("Hola", nombre, "y su edad es", edad)
+
+# print("ingrese su nombre")
+# nombre=input()
+# print("ingrese su edad")
+# edad=input()
+# #ejemplo de concatenacion
+# print("Hola", nombre, "y su edad es", edad)
+
+
+# n1=int(input("ingrese un numero "))
+# n2=int(input("ingrese un numero "))
+
+# print(n1+n2)
+
+n1=int(input("ingrese un numero "))
+n2=int(input("ingrese un numero "))
+
+print(n1*n2)
+
